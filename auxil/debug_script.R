8d64499eb954be9127375fdd18c73e3b25922139
Karl Emmert-Fees <karl.emmert-fees@tum.de>
2022-07-21 12:49:37 +0100

    Added debug script

---
+++ auxil/debug_script.R
@@ -0,0 +1,6 @@
+self <- IMPACTncd$.__enclos_env__$self
+private <- IMPACTncd$.__enclos_env__$private
+
+x <- IMPACTncd$diseases$stroke
+self <- x$.__enclos_env__$self
+private <- x$.__enclos_env__$private
--- auxil/simulation.R
+++ auxil/simulation.R
@@ -6,23 +6,17 @@ IMPACTncd <- Simulation$new("./inputs/sim_design.yaml")
 
 g <- IMPACTncd$get_causal_structure(print_plot = T)
 
-mc_iterations <- 5
-
-for(i in 1:(mc_iterations*5)){
-  SynthPop$new(i, design)
-}
-
 scenario_fn <- function(sp) NULL
 
 IMPACTncd$
   del_logs()$
   del_outputs()$
-  run(7:8, multicore = FALSE, "Baseline", perc_change_m0 = 0.97)
+  run(3:4, multicore = FALSE, "Baseline", perc_change_m0 = 0.97)
 
 source("./auxil/scenarios.R")
 
 IMPACTncd$
-  run(7:8, multicore = FALSE, "Main Scenario", perc_change_m0 = 0.97)$
+  run(3:4, multicore = FALSE, "Main Scenario", perc_change_m0 = 0.97)$
   export_summaries(multicore = TRUE)
 
 # IMPACTncd$export_summaries(multicore = TRUE)
