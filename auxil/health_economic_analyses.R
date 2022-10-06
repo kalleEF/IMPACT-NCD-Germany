475c16f249768adf0967674660e361b5b62f6fd5
Karl Emmert-Fees <karl.emmert-fees@tum.de>
2022-10-06 17:41:47 +0100

    Implemented uncertainty in health utility

--- auxil/health_economic_analyses.R
+++ auxil/health_economic_analyses.R
@@ -3,43 +3,64 @@ library(fst)
 library(data.table)
 library(ggplot2)
 
+mc_s <- 10000 # Number of MC samples
 
-cea_data <- CJ(age = seq(20, 100, 1),
-               sex = c(1, 0), # 1 = women, 0 = men
-               t2dm_prvl = c(1, 0),
-               chd_prvl = c(1, 0),
-               stroke_prvl = c(1, 0))
+n_s <- 8 # Number of utility parameters
 
-#### Health utility values ----
-# Source: Laxy et al. 2021 Value in Health #
+util_data <- data.table(NULL)
 
-## Age and sex utility decrements #
-cea_data[, util := 1.187 + (age * -0.003) + (sex * -0.003)]
+for(i in 1:mc_s){
 
-## Disease-specific utility decrements #
-# Type 2 Diabetes
-cea_data[, util := fifelse(t2dm_prvl == 1 & chd_prvl == 0 & stroke_prvl == 0,
-                           util - 0.028, # Only Diabetes
-                           fifelse(t2dm_prvl == 1 & chd_prvl == 1 & stroke_prvl == 0,
-                                   util - 0.028, ## TBD! Diabetes and CHD
-                                   fifelse(t2dm_prvl == 1 & chd_prvl == 0 & stroke_prvl == 1,
-                                           util - 0.122, # Diabetes and Stroke
-                                           fifelse(t2dm_prvl == 1 & chd_prvl == 1 & stroke_prvl == 1,
-                                                   util - 0.122, util))))] # Diabetes, CHD and Stroke
+  ## Ensure replicability #  
+  set.seed(i + 1337)
+  
+  ## Draw quantiles #
+  quantiles <- runif(n_s, min = 0, max = 1) * 0.999
 
-# Coronary Heart Disease and Stroke
-cea_data[, util := fifelse(t2dm_prvl == 0 & chd_prvl == 1 & stroke_prvl == 0,
-                           util - 0.028, # Only CHD
-                           fifelse(t2dm_prvl == 0 & chd_prvl == 1 & stroke_prvl == 1,
-                                   util - 0.070, ## TBD! CHD and Stroke
-                                   fifelse(t2dm_prvl == 0 & chd_prvl == 0 & stroke_prvl == 1,
-                                           util -0.070, util)))] # Only Stroke 
+  #### Health utility values ----
+  # Source: Laxy et al. 2021 Value in Health #
+  
+  xx <- CJ(age = seq(20, 100, 1),
+           sex = c(1, 0), # 1 = women, 0 = men
+           t2dm_prvl = c(1, 0),
+           chd_prvl = c(1, 0),
+           stroke_prvl = c(1, 0))
+  
+  ## Age and sex utility decrements #
+  xx[, util := qnorm(quantiles[1], mean = 1.187, sd = 0.019) +
+              (age * qnorm(quantiles[2], mean = -0.003, sd = 0.0001)) +
+              (sex * qnorm(quantiles[3], mean = -0.029, sd = 0.004))]
+  
+  ## BMI decrement #
+  xx[, util_bmi := qnorm(quantiles[4], mean = -0.003, sd = 0.0001)]
+  
+  ## Disease-specific utility decrements #
+  # Type 2 Diabetes
+  xx[, util_disease := fifelse(t2dm_prvl == 1 & chd_prvl == 0 & stroke_prvl == 0,
+                               qnorm(quantiles[5], mean = -0.028, sd = 0.014), # Only Diabetes
+                       fifelse(t2dm_prvl == 1 & chd_prvl == 1 & stroke_prvl == 0,
+                               qnorm(quantiles[5], mean = -0.028, sd = 0.014), ## TBD! Diabetes and CHD
+                       fifelse(t2dm_prvl == 1 & chd_prvl == 0 & stroke_prvl == 1,
+                               qnorm(quantiles[6], mean = -0.122, sd = 0.018), # Diabetes and Stroke
+                       fifelse(t2dm_prvl == 1 & chd_prvl == 1 & stroke_prvl == 1,
+                               qnorm(quantiles[6], mean = -0.122, sd = 0.018), 0))))] # Diabetes, CHD and Stroke
+  
+  # Coronary Heart Disease and Stroke
+  xx[, util_disease := fifelse(t2dm_prvl == 0 & chd_prvl == 1 & stroke_prvl == 0,
+                               qnorm(quantiles[7], mean = -0.028, sd = 0.010), # Only CHD
+                       fifelse(t2dm_prvl == 0 & chd_prvl == 1 & stroke_prvl == 1,
+                               qnorm(quantiles[8], mean = -0.070, sd = 0.010), ## TBD! CHD and Stroke
+                       fifelse(t2dm_prvl == 0 & chd_prvl == 0 & stroke_prvl == 1,
+                               qnorm(quantiles[8], mean = -0.070, sd = 0.010), util_disease)))] # Only Stroke 
+  
+  xx[, `:=`(sex = ifelse(sex == 1, "women", "men"),
+            mc = i)]
+  
+  util_data <- rbind(xx, util_data)
+  
+}
 
-
-
-
-
-
+write_fst(util_data, "./inputs/other_parameters/health_utility.fst")
 
 
 
