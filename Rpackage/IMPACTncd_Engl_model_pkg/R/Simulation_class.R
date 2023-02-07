9057dda14aab7b8ed13801e15761ee513466d2ca
Karl Emmert-Fees <karl.emmert-fees@tum.de>
2023-02-07 11:50:16 +0100

    Implement more efficient health utility estimation

--- Rpackage/IMPACTncd_Engl_model_pkg/R/Simulation_class.R
+++ Rpackage/IMPACTncd_Engl_model_pkg/R/Simulation_class.R
@@ -1056,9 +1056,21 @@ Simulation <-
           
           ### New utility estimation ###
 
-          cov <- read_fst("./inputs/other_parameters/health_util_covariance.fst", as.data.table = TRUE)
+          cov <- read_fst("./inputs/other_parameters/health_util_covariance.fst")
           est <- read_fst("./inputs/other_parameters/health_util_estimates.fst", as.data.table = TRUE)
-
+          
+          cov_long <- (NULL)
+          
+          for(i in 1:nrow(cov)){
+            
+            tmp <- t(cov[, i])
+            
+            colnames(tmp) <- paste0("cov", seq(1, length(tmp), 1), "_", i)
+            
+            cov_long <- cbind(cov_long, tmp)
+            
+          }
+          
           lc[, `:=`(Intercept = 1,
                     sex_num = ifelse(sex == "men", 0, 1),
                     copd = 0,
@@ -1075,33 +1087,69 @@ Simulation <-
                     chd = ifelse(chd_prvl > 0, 1, 0),
                     t2dm_chd = ifelse(t2dm_prvl > 0 & chd_prvl > 0, 1, 0))]
 
-          est_utility <- function(Intercept, age, sex_num, bmi_curr_xps,
-                                  copd, cancer, asthma, bronchitis, t2dm, hypertension, t2dm_hypt,
-                                  stroke, t2dm_stroke, arrhythmia, t2dm_arythm, chd, t2dm_chd){
-
-            utility_se <- sqrt(
-              c(Intercept, age, sex_num, bmi_curr_xps,
-                copd, cancer, asthma, bronchitis, t2dm, hypertension, t2dm_hypt,
-                stroke, t2dm_stroke, arrhythmia, t2dm_arythm, chd, t2dm_chd)
-
-              %*%
-
-                as.matrix(cov)
-
-              %*%
-
-                c(Intercept, age, sex_num, bmi_curr_xps,
-                  copd, cancer, asthma, bronchitis, t2dm, hypertension, t2dm_hypt,
-                  stroke, t2dm_stroke, arrhythmia, t2dm_arythm, chd, t2dm_chd)
-            )
-
-            return(utility_se)
-
+          
+          j <- 0
+          
+          for(i in seq(1, ncol(cov_long), 17)){
+            
+            j <- j + 1
+            
+            cols <- seq(i, i + 16, 1)
+            
+            row <- seq(1, length(cols), 1)
+            
+            lc[, (colnames(cov_long)[cols]) := as.data.table(t(cov_long[cols]))]
+            
+            lc[, (paste0("tmp", i)) := Intercept * get(paste0("cov", row[1], "_", j)) + age * get(paste0("cov", row[2], "_", j)) +
+                 sex_num * get(paste0("cov", row[3], "_", j)) + bmi_curr_xps * get(paste0("cov", row[4], "_", j)) +
+                 copd * get(paste0("cov", row[5], "_", j)) + cancer * get(paste0("cov", row[6], "_", j)) +
+                 asthma * get(paste0("cov", row[7], "_", j)) + bronchitis * get(paste0("cov", row[8], "_", j)) +
+                 t2dm * get(paste0("cov", row[9], "_", j)) + hypertension * get(paste0("cov", row[10], "_", j)) +
+                 t2dm_hypt * get(paste0("cov", row[11], "_", j)) + stroke * get(paste0("cov", row[12], "_", j)) +
+                 t2dm_stroke * get(paste0("cov", row[13], "_", j)) + arrhythmia * get(paste0("cov", row[14], "_", j)) +
+                 t2dm_arythm * get(paste0("cov", row[15], "_", j)) + chd * get(paste0("cov", row[16], "_", j)) +
+                 t2dm_chd * get(paste0("cov", row[17], "_", j))]
+            
+            lc[, (grep("cov", names(lc))) := NULL]
+            
           }
-
-          lc[, utility_se := mapply(est_utility, Intercept, age, sex_num, bmi_curr_xps,
-                                    copd, cancer, asthma, bronchitis, t2dm, hypertension, t2dm_hypt,
-                                    stroke, t2dm_stroke, arrhythmia, t2dm_arythm, chd, t2dm_chd)]
+          
+          rm(j)
+          
+          lc[, utility_se := sqrt(Intercept * tmp1 + age * tmp18 + sex_num * tmp35 +
+                                        bmi_curr_xps * tmp52 + copd * tmp69 + cancer * tmp86 +
+                                        asthma * tmp103 + bronchitis * tmp120 + t2dm * tmp137 +
+                                        hypertension * tmp154 + t2dm_hypt * tmp171 +
+                                        stroke * tmp188 + t2dm_stroke * tmp205 + arrhythmia * tmp222 +
+                                        t2dm_arythm * tmp239 + chd * tmp256 + t2dm_chd * tmp273)][, (grep("tmp", names(lc))) := NULL]
+          # 
+          # est_utility <- function(Intercept, age, sex_num, bmi_curr_xps,
+          #                         copd, cancer, asthma, bronchitis, t2dm, hypertension, t2dm_hypt,
+          #                         stroke, t2dm_stroke, arrhythmia, t2dm_arythm, chd, t2dm_chd){
+          # 
+          #   utility_se <- sqrt(
+          #     c(Intercept, age, sex_num, bmi_curr_xps,
+          #       copd, cancer, asthma, bronchitis, t2dm, hypertension, t2dm_hypt,
+          #       stroke, t2dm_stroke, arrhythmia, t2dm_arythm, chd, t2dm_chd)
+          # 
+          #     %*%
+          # 
+          #       as.matrix(cov)
+          # 
+          #     %*%
+          # 
+          #       c(Intercept, age, sex_num, bmi_curr_xps,
+          #         copd, cancer, asthma, bronchitis, t2dm, hypertension, t2dm_hypt,
+          #         stroke, t2dm_stroke, arrhythmia, t2dm_arythm, chd, t2dm_chd)
+          #   )
+          # 
+          #   return(utility_se)
+          # 
+          # }
+          # 
+          # lc[, utility_se := mapply(est_utility, Intercept, age, sex_num, bmi_curr_xps,
+          #                           copd, cancer, asthma, bronchitis, t2dm, hypertension, t2dm_hypt,
+          #                           stroke, t2dm_stroke, arrhythmia, t2dm_arythm, chd, t2dm_chd)]
 
           lc[, (names(est)) := est]
 
