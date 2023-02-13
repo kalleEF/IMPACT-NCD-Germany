171c104ed85f8e40a1ec6c29a071679bd6c287ed
Karl Emmert-Fees <karl.emmert-fees@tum.de>
2023-02-13 10:57:53 +0100

    Updated PRIMEtime output processing

--- auxil/process_out_PRIMEtime.R
+++ auxil/process_out_PRIMEtime.R
@@ -1,7 +1,10 @@
 
+### Extract and process outputs from PRIMEtime Excel Model ----
 
 library(openxlsx)
 library(data.table)
+
+# Read relevant rows and columns from Excel workbook
 
 prime <- read.xlsx("G:/Meine Ablage/PhD/Publications/2021_Diet_simulation_modeling_Germany/Model/PRIMEtime-CE/02-10-2023_PRIMEtimeGermany_cross_validation.xlsm",
             sheet = "OutputData",
@@ -10,43 +13,47 @@ prime <- read.xlsx("G:/Meine Ablage/PhD/Publications/2021_Diet_simulation_modeli
 
 prime <- data.table(prime)
 
-prime <- prime[6:5005]
+prime <- prime[6:5005, -1]
 
 prime <- na.omit(prime)
 
+mc_ <- nrow(prime)
+
+col_ids <- seq(1, ncol(prime) + 1, 92)
+
 # Split by intervention
 
-prime_intv_1 <- prime[, 2:93]
-prime_intv_2 <- prime[, 94:185]
-prime_intv_3 <- prime[, 186:277]
-prime_intv_4 <- prime[, 278:369]
+prime_intv_1 <- prime[, 1:(col_ids[2]-1)]
+prime_intv_2 <- prime[, col_ids[2]:(col_ids[3]-1)]
+prime_intv_3 <- prime[, col_ids[3]:(col_ids[4]-1)]
+prime_intv_4 <- prime[, col_ids[4]:(col_ids[5]-1)]
 
 # Split by sex
 
 prime_intv_1_male <- prime_intv_1[, 1:30]
-prime_intv_1_male[, `:=`(sex = "male", scenario = "sc1", mc = 1:100)]
+prime_intv_1_male[, `:=`(sex = "male", scenario = "sc1", mc = 1:mc_)]
 
 prime_intv_2_male <- prime_intv_2[, 1:30]
-prime_intv_2_male[, `:=`(sex = "male", scenario = "sc2", mc = 1:100)]
+prime_intv_2_male[, `:=`(sex = "male", scenario = "sc2", mc = 1:mc_)]
 
 prime_intv_3_male <- prime_intv_3[, 1:30]
-prime_intv_3_male[, `:=`(sex = "male", scenario = "sc3", mc = 1:100)]
+prime_intv_3_male[, `:=`(sex = "male", scenario = "sc3", mc = 1:mc_)]
 
 prime_intv_4_male <- prime_intv_4[, 1:30]
-prime_intv_4_male[, `:=`(sex = "male", scenario = "sc4", mc = 1:100)]
+prime_intv_4_male[, `:=`(sex = "male", scenario = "sc4", mc = 1:mc_)]
 
 
 prime_intv_1_female <- prime_intv_1[, 31:60]
-prime_intv_1_female[, `:=`(sex = "female", scenario = "sc1", mc = 1:100)]
+prime_intv_1_female[, `:=`(sex = "female", scenario = "sc1", mc = 1:mc_)]
 
 prime_intv_2_female <- prime_intv_2[, 31:60]
-prime_intv_2_female[, `:=`(sex = "female", scenario = "sc2", mc = 1:100)]
+prime_intv_2_female[, `:=`(sex = "female", scenario = "sc2", mc = 1:mc_)]
 
 prime_intv_3_female <- prime_intv_3[, 31:60]
-prime_intv_3_female[, `:=`(sex = "female", scenario = "sc3", mc = 1:100)]
+prime_intv_3_female[, `:=`(sex = "female", scenario = "sc3", mc = 1:mc_)]
 
 prime_intv_4_female <- prime_intv_4[, 31:60]
-prime_intv_4_female[, `:=`(sex = "female", scenario = "sc4", mc = 1:100)]
+prime_intv_4_female[, `:=`(sex = "female", scenario = "sc4", mc = 1:mc_)]
 
 
 # rbind datasets
