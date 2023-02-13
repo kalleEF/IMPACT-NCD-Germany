2f54eecfc4bf7c25c66199fa3b7e111f57528de9
Karl Emmert-Fees <karl.emmert-fees@tum.de>
2023-02-13 10:54:56 +0100

    Added script to estimate PRIMEtime input distributions

--- auxil/estimate_distributions_PRIMEtime.R
+++ auxil/estimate_distributions_PRIMEtime.R
@@ -4,20 +4,34 @@
 library(data.table)
 library(foreach)
 library(Hmisc)
+library(CKutils)
+library(fst)
+
+doParallel::registerDoParallel(32)
 
 # Aggregate SynthPops # ----
 
-synthpops <- grep(pattern = ".fst", list.files("media/php-workstation/Storage 1/IMPACT_Storage/inputs/synthpop/"), value = TRUE)
+synthpops <- grep(pattern = ".fst", list.files("/media/php-workstation/Storage_1/IMPACT_Storage/inputs/synthpop/"), value = TRUE)
 
-ids <- sample(1:length(synthpops), 500, replace = FALSE)
+ids <- sample(1:length(synthpops), 300, replace = FALSE)
+
+tt <- read_fst("./inputs/pop_projections/German_pop_combined.fst", as.data.table = TRUE)
 
 dat <- foreach(i = seq(synthpops)[ids],
                .packages = c("fst", "data.table")) %dopar% {
                  
-                 xx <- read_fst(paste0("media/php-workstation/Storage 1/IMPACT_Storage/inputs/synthpop/", synthpops[i]), as.data.table = TRUE)[
+                 xx <- read_fst(paste0("/media/php-workstation/Storage_1/IMPACT_Storage/inputs/synthpop/", synthpops[i]), as.data.table = TRUE)[
                    year >= 13, .(year, age, sex,
                                  bmi_curr_xps, ssb_curr_xps, juice_curr_xps,
-                                 sugar_per_ssb, sugar_per_juice, wt)]
+                                 sugar_per_ssb, sugar_per_juice)]
+                 
+                 tt <- tt[between(age, min(xx$age), max(xx$age)) &
+                            between(year, min(xx$year), max(xx$year)),
+                          .(pops = sum(pops)), keyby = .(year, age, sex)]
+                 xx[, wt_immrtl := .N, by = .(year, age, sex)]
+                 absorb_dt(xx, tt)
+                 xx[, wt := pops / (wt_immrtl * 1)]
+                 xx[, pops := NULL]
                  
                  return(xx)
                  
@@ -51,7 +65,7 @@ bmi_synthpop_table[, sex := ifelse(sex == "men", "male", "female")]
 
 setkey(bmi_synthpop_table, sex, year)
 
-fwrite(bmi_synthpop_table, "media/php-workstation/Storage 1/IMPACT_Storage/PRIMEtime_export/bmi_synthpop_input_2013_2035.csv", sep = ";", dec = ".")
+fwrite(bmi_synthpop_table, "/media/php-workstation/Storage_1/IMPACT_Storage/PRIMEtime_export/bmi_synthpop_input_2013_2035.csv", sep = ";", dec = ".")
 
 
 
@@ -78,7 +92,7 @@ ssb_synthpop_table[, sex := ifelse(sex == "men", "male", "female")]
 
 setkey(ssb_synthpop_table, sex)
 
-fwrite(ssb_synthpop_table, "media/php-workstation/Storage 1/IMPACT_Storage/PRIMEtime_export/ssb_synthpop_input_2013.csv", sep = ";", dec = ".")
+fwrite(ssb_synthpop_table, "/media/php-workstation/Storage_1/IMPACT_Storage/PRIMEtime_export/ssb_synthpop_input_2013.csv", sep = ";", dec = ".")
 
 
 
@@ -105,7 +119,7 @@ juice_synthpop_table[, sex := ifelse(sex == "men", "male", "female")]
 
 setkey(juice_synthpop_table, sex)
 
-fwrite(juice_synthpop_table, "media/php-workstation/Storage 1/IMPACT_Storage/PRIMEtime_export/juice_synthpop_input_2013.csv", sep = ";", dec = ".")
+fwrite(juice_synthpop_table, "/media/php-workstation/Storage_1/IMPACT_Storage/PRIMEtime_export/juice_synthpop_input_2013.csv", sep = ";", dec = ".")
 
 
 ## SSB sugar table ## ----
@@ -119,7 +133,7 @@ dat_sd <- dat_bind[, lapply(.SD, wtd.var, weights = wt), # weighted_sd
                    .SDcols = "sugar_per_ssb",
                    by = .(sex, age)]
 setnames(dat_sd, "sugar_per_ssb", "SSB_sugar_sd")
-dat_sd[, SSb_sugar_sd := sqrt(SSB_sugar_sd)]
+dat_sd[, SSB_sugar_sd := sqrt(SSB_sugar_sd)]
 
 ssb_sugar_table <- merge(dat_mean, dat_sd, by = c("sex", "age"))
 
@@ -131,7 +145,7 @@ ssb_sugar_table[, sex := ifelse(sex == "men", "male", "female")]
 
 setkey(ssb_sugar_table, sex)
 
-fwrite(ssb_sugar_table, "media/php-workstation/Storage 1/IMPACT_Storage/PRIMEtime_export/ssb_sugar_input_2013.csv", sep = ";", dec = ".")
+fwrite(ssb_sugar_table, "/media/php-workstation/Storage_1/IMPACT_Storage/PRIMEtime_export/ssb_sugar_input_2013.csv", sep = ";", dec = ".")
 
 
 ## Juice sugar table ## ----
@@ -157,7 +171,7 @@ juice_synthpop_table[, sex := ifelse(sex == "men", "male", "female")]
 
 setkey(juice_sugar_table, sex)
 
-fwrite(juice_sugar_table, "media/php-workstation/Storage 1/IMPACT_Storage/PRIMEtime_export/juice_sugar_input_2013.csv", sep = ";", dec = ".")
+fwrite(juice_sugar_table, "/media/php-workstation/Storage_1/IMPACT_Storage/PRIMEtime_export/juice_sugar_input_2013.csv", sep = ";", dec = ".")
 
 
 
