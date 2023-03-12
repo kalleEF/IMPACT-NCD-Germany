dab62bc3e45948e6407c52514357966f383f7cc4
Karl Emmert-Fees <karl.emmert-fees@tum.de>
2023-03-12 15:57:32 +0100

    Added new reformulation scenario

---
+++ analysis/2_additional_reformulation_scenarios_script.R
@@ -0,0 +1,242 @@
+
+#### Analysis script IMPACT NCD Germany SSB Tax modeling ----
+
+# Load packages
+source("./global.R")
+
+# Load scenario and sensitivity analyses functions
+source("./auxil/scenarios.R")
+source("./auxil/sensitivity_analyses.R")
+
+# Initiate .Random.seed for safety
+runif(1)
+
+# New runs?
+new_runs <- TRUE
+
+if(new_runs){
+  
+  # Create batches for batched simulation
+  batch_size <- 10
+  iterations <- 500
+  first_iteration <- 1
+  batches <- split(seq(first_iteration, iterations + first_iteration - 1),
+                   f = findInterval(seq(first_iteration, iterations + first_iteration - 1),
+                                    vec = seq(first_iteration, iterations + first_iteration - 1, batch_size)))
+}
+
+# Logic to ensure correct RR files are loaded
+load_RRs <- function(RR){
+  
+  if(!is.character(RR)) stop("RR needs to be a character vector of IMPACT RR files")
+  
+  # Load needed files
+  
+  for(file in unique(RR)){
+    
+    if(file.exists(paste0("./inputs/RR/", file, ".csvy"))){
+      
+      print(paste0("RR file ", file, " is already loaded"))
+    
+      } else if(file.exists(paste0("./inputs/RR/unused/", file, ".csvy"))) {
+      
+        file.copy(from = paste0("./inputs/RR/unused/", file, ".csvy"),
+                  to = paste0("./inputs/RR/", file, ".csvy"))
+
+        file.remove(paste0("./inputs/RR/unused/", file, ".csvy"))
+
+        print(paste0("RR file ", file, " has been loaded"))
+
+      }
+  }
+  
+  # Unload un-needed files
+  
+  unload_files <- grep(".csvy", outersect(paste0(RR, ".csvy"), list.files("./inputs/RR/")), value = TRUE)
+  
+  unload_files <- gsub(".csvy", "", unload_files)
+  
+  for(file in unique(unload_files)){
+    
+    if(file.exists(paste0("./inputs/RR/", file, ".csvy"))){
+      
+      file.copy(from = paste0("./inputs/RR/", file, ".csvy"),
+                to = paste0("./inputs/RR/unused/", file, ".csvy"))
+      
+      file.remove(paste0("./inputs/RR/", file, ".csvy"))
+      
+      print(paste0("RR file ", file, " has been unloaded"))
+      
+    } else if(file.exists(paste0("./inputs/RR/unused/", file, ".csvy"))) {
+      
+      print(paste0("RR file ", file, " is already unloaded"))
+      
+    }
+  }
+  
+}
+
+
+### Main analysis: Including direct SSB effects ----
+
+load_RRs(c("bmi~chd", "bmi~obesity", "bmi~stroke", "bmi~t2dm",
+           "ssb~chd", "ssb~t2dm",
+           "t2dm_prvl~chd", "t2dm_prvl~nonmodelled", "t2dm_prvl~stroke"))
+
+analysis_name <- "with_direct_SSB_effects_reform"
+
+IMPACTncd <- Simulation$new("./inputs/sim_design.yaml", analysis_name)
+
+if(new_runs){
+  for(i in batches){
+    
+    scenario_fn <- scenario_0_fn
+    
+    IMPACTncd$
+      run(i, multicore = TRUE, "sc0", m_zero_trend = -0.03, p_zero_trend = 0)
+    
+    scenario_fn <- scenario_3_fn
+    
+    IMPACTncd$
+      run(i, multicore = TRUE, "sc32", m_zero_trend = -0.03, p_zero_trend = 0)
+    
+    scenario_fn <- scenario_4_fn
+    
+    IMPACTncd$
+      run(i, multicore = TRUE, "sc42", m_zero_trend = -0.03, p_zero_trend = 0)
+    
+  }
+}
+
+IMPACTncd$export_summaries(multicore = TRUE)
+
+
+### Alternative analysis: Excluding direct SSB effects ----
+
+load_RRs(c("bmi~chd", "bmi~obesity", "bmi~stroke", "bmi~t2dm",
+           "t2dm_prvl~chd", "t2dm_prvl~nonmodelled", "t2dm_prvl~stroke"))
+
+analysis_name <- "without_direct_SSB_effects_reform"
+
+IMPACTncd <- Simulation$new("./inputs/sim_design.yaml", analysis_name)
+
+if(new_runs){
+  for(i in batches){
+    
+    scenario_fn <- scenario_0_fn
+    
+    IMPACTncd$
+      run(i, multicore = TRUE, "sc0", m_zero_trend = -0.03, p_zero_trend = 0)
+    
+    scenario_fn <- scenario_3_fn
+    
+    IMPACTncd$
+      run(i, multicore = TRUE, "sc32", m_zero_trend = -0.03, p_zero_trend = 0)
+    
+    scenario_fn <- scenario_4_fn
+    
+    IMPACTncd$
+      run(i, multicore = TRUE, "sc42", m_zero_trend = -0.03, p_zero_trend = 0)
+    
+  }
+}
+
+IMPACTncd$export_summaries(multicore = TRUE)
+
+
+### Reformulation sensitivity analyses (no changes in modelled pathways) ----
+
+load_RRs(c("bmi~chd", "bmi~obesity", "bmi~stroke", "bmi~t2dm",
+           "ssb~chd", "ssb~t2dm",
+           "t2dm_prvl~chd", "t2dm_prvl~nonmodelled", "t2dm_prvl~stroke"))
+
+analysis_name <- "sensitivity_analyses_reform"
+
+IMPACTncd <- Simulation$new("./inputs/sim_design.yaml", analysis_name)
+
+if(new_runs){
+  for(i in batches){
+    
+    scenario_fn <- scenario_0_fn
+    
+    IMPACTncd$
+      run(i, multicore = TRUE, "sc0", m_zero_trend = -0.03, p_zero_trend = 0)
+    
+    scenario_fn <- scenario_sens_4_fn
+    
+    IMPACTncd$
+      run(i, multicore = TRUE, "sens_42", m_zero_trend = -0.03, p_zero_trend = 0)
+  
+    scenario_fn <- scenario_sens_6_fn
+    
+    IMPACTncd$
+      run(i, multicore = TRUE, "sens_6", m_zero_trend = -0.03, p_zero_trend = 0)
+      
+  }
+}
+
+IMPACTncd$export_summaries(multicore = TRUE)
+
+
+
+### Sensitivity analyses: Discounting ----
+
+if(new_runs){
+  
+  load_RRs(c("bmi~chd", "bmi~obesity", "bmi~stroke", "bmi~t2dm",
+             "ssb~chd", "ssb~t2dm",
+             "t2dm_prvl~chd", "t2dm_prvl~nonmodelled", "t2dm_prvl~stroke"))
+  
+  analysis_name <- "with_direct_SSB_effects_reform"
+  
+  IMPACTncd <- Simulation$new("./inputs/sim_design.yaml", analysis_name)
+  
+  ## Rename original files with 3% discount rate:
+  file.rename(from = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/cea_results.csv.gz"),
+              to = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/cea_results",
+                          gsub("0.0", "", as.character(IMPACTncd$design$sim_prm$discount_rate)), ".csv.gz"))
+  
+  file.rename(from = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/health_economic_results.csv.gz"),
+              to = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/health_economic_results",
+                          gsub("0.0", "", as.character(IMPACTncd$design$sim_prm$discount_rate)), ".csv.gz"))
+  
+  ## Apply 1% discount rate:
+  IMPACTncd$design$sim_prm$discount_rate <- 0.01
+  IMPACTncd$export_summaries(multicore = TRUE, type = "cea")
+  
+  file.rename(from = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/cea_results.csv.gz"),
+              to = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/cea_results",
+                          gsub("0.0", "", as.character(IMPACTncd$design$sim_prm$discount_rate)), ".csv.gz"))
+  
+  file.rename(from = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/health_economic_results.csv.gz"),
+              to = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/health_economic_results",
+                          gsub("0.0", "", as.character(IMPACTncd$design$sim_prm$discount_rate)), ".csv.gz"))
+  
+  ## Apply 5% discount rate:
+  IMPACTncd$design$sim_prm$discount_rate <- 0.05
+  IMPACTncd$export_summaries(multicore = TRUE, type = "cea")
+  
+  file.rename(from = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/cea_results.csv.gz"),
+              to = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/cea_results",
+                          gsub("0.0", "", as.character(IMPACTncd$design$sim_prm$discount_rate)), ".csv.gz"))
+  
+  file.rename(from = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/health_economic_results.csv.gz"),
+              to = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/health_economic_results",
+                          gsub("0.0", "", as.character(IMPACTncd$design$sim_prm$discount_rate)), ".csv.gz"))
+  
+  ## Apply 10% discount rate:
+  IMPACTncd$design$sim_prm$discount_rate <- 0.10
+  IMPACTncd$export_summaries(multicore = TRUE, type = "cea")
+  
+  file.rename(from = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/cea_results.csv.gz"),
+              to = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/cea_results",
+                          gsub("0.1", "10", as.character(IMPACTncd$design$sim_prm$discount_rate)), ".csv.gz"))
+  
+  file.rename(from = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/health_economic_results.csv.gz"),
+              to = paste0(IMPACTncd$design$sim_prm$output_dir, "/", analysis_name, "/summaries/health_economic_results",
+                          gsub("0.1", "10", as.character(IMPACTncd$design$sim_prm$discount_rate)), ".csv.gz"))
+
+}
+
+
+
--- auxil/sensitivity_analyses.R
+++ auxil/sensitivity_analyses.R
@@ -268,3 +268,52 @@ scenario_sens_5_fn <- function(sp) {
   sp$pop[, c("juice_delta_xps") := 0] 
   
 }
+
+
+### Sensitivity analysis 6 - Volunatry industry reformulation by 2% in 6 years ----
+
+scenario_sens_6_fn <- function(sp) {
+  
+  # Set scenario variables #
+  ref <- 0.02/6
+  
+  tbl <- read_fst("./inputs/other_parameters/bmi_lag.fst", as.data.table = TRUE)
+  bmi_lag <- as.numeric(tbl[mc == sp$mc_aggr, "bmi_lag"]) # Lag until full BMI reduction is achieved
+  bmi_steps <- 1/bmi_lag
+  
+  tbl <- read_fst("./inputs/other_parameters/sugar_rr.fst", as.data.table = TRUE)
+  sugar_rr_low <- as.numeric(tbl[mc == sp$mc_aggr, "sugar_rr_low"]) # Change in BMI due to sugar by BMI ><25
+  sugar_rr_high <- as.numeric(tbl[mc == sp$mc_aggr, "sugar_rr_high"])
+  
+  sp$pop[, ref_mod := 0]
+  sp$pop[year > (IMPACTncd$design$sim_prm$init_year_intv - 2000), ref_mod := seq(1, .N, 1), by = "pid"]
+  sp$pop[, ref_mod := 1 - (ref_mod * ref)]
+  
+  sp$pop[year > (IMPACTncd$design$sim_prm$init_year_intv - 2000), sugar_per_ssb_new := sugar_per_ssb * ref_mod]
+  
+  # Change in consumption of sugar from SSBs after tax #
+  sp$pop[, sugar_delta := ssb_sugar - (ssb_curr_xps * sugar_per_ssb_new)]
+  
+  # Recalculate SSB consumption to account for direct effect of reduced sugar SSBs #
+  sp$pop[, ssb_curr_xps := ssb_curr_xps - sugar_delta / sugar_per_ssb]
+  
+  
+  # Change in BMI after tax #
+  sp$pop[, bmi_delta := fifelse(bmi_curr_xps < 25,
+                                sugar_delta * sugar_rr_low,
+                                sugar_delta * sugar_rr_high)]
+  
+  # Lagged effect of BMI #
+  sp$pop[, bmi_mod := 0]
+  sp$pop[year > (IMPACTncd$design$sim_prm$init_year_intv - 2000), bmi_mod := fifelse(year > (IMPACTncd$design$sim_prm$init_year_intv - 2000) & year <= ((IMPACTncd$design$sim_prm$init_year_intv - 2000) + bmi_lag),
+                                                                                     (year - (IMPACTncd$design$sim_prm$init_year_intv - 2000)) * bmi_steps,
+                                                                                     1)]
+  
+  # New BMI under taxation scenario #
+  sp$pop[year > (IMPACTncd$design$sim_prm$init_year_intv - 2000), bmi_curr_xps := bmi_curr_xps - (bmi_delta * bmi_mod)]
+  
+  # Delete unnecessary variables from synthpop #
+  sp$pop[, c("ref_mod", "bmi_mod", "sugar_per_ssb_new") := NULL]
+  sp$pop[, c("ssb_delta_xps", "juice_delta_xps") := 0] 
+  
+}
